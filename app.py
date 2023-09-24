e52ecdfdfc1170633c4f5d60fad1ea805f498d52
elijahnewton <65656407+elijahnewton@users.noreply.github.com>
2023-09-25 02:22:39 +0300

    Update app.py
    updating...

--- app.py
+++ app.py
@@ -338,7 +338,7 @@ def login():
                 stored_password_hash = user_data.get('password_hash', None)
 
                 if stored_password_hash and check_password_hash(stored_password_hash, password):
-                    user = User(user_id=user_data['sk'], username=user_data['username'], pk=user_data['pk'], password_hash=stored_password_hash, role_id=user_data['role_id'])
+                    user = User(user_id=user_data['sk'], username=user_data['username'], pk=user_data['pk'], password_hash=stored_password_hash, role_id=user_data['sk'])
                     login_user(user)
                     flash('Logged In successfully!', 'success')
                     return redirect('/')
